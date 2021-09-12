5a23c674f6671f6abdeb4408c5ff56d67cc9014c
KBHeid <kbheid@live.com>
2021-09-12 11:13:15 -0600

    Closes #5, #1

---
+++ js/collision.js
@@ -0,0 +1,26 @@
+function collectStar(player, star){
+	star.disableBody(true, true);
+
+	score += 10;
+	scoreText.setText('Score: ' + score);
+
+	if(stars.countActive(true) === 0){
+		stars.children.iterate(function(child){
+			child.enableBody(true, child.x, 0, true, true);
+		});
+		var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+		var bomb = bombs.create(x, 16, 'bomb');
+		bomb.setBounce(1);
+		//bomb.setCollideWorldBounds(true);
+		bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+	}
+}
+
+function hitBomb(player, bomb){
+	this.physics.pause();
+	player.setTint(0xff0000);
+	player.anims.play('turn');
+	gameOver = true;
+	scoreText.setText('Score: ' + score + '\nHit R to restart');
+}
+
